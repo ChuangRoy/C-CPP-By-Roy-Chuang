d9fd9cb82e1ef0df5a63de461049f0870278ef78
Roy Chuang <s041516@@boun.cr>
2021-07-10 17:51:36 +0800

    change to clang compiler

--- Hello_World.c
+++ Hello_World.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    printf("哈囉大家好\n");
-    system("pause");
-    return 0;
+int main() {
+  printf("Hello world!\n");
+  printf("哈囉大家好\n");
+  system("pause");
+  return 0;
 }
